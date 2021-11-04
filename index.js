0781124ccbbb333b2e4d3172c4bc966b68fd596a
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-04 12:37:02 -0700

    Route parameters

--- index.js
+++ index.js
@@ -14,13 +14,18 @@ app.get('/', (request, response) => {
   response.send('I have been getted again')
 });
 
+
 app.get('/api/courses', (request, response) => {
   response.send([1,2,3]); //in place of a database call we are using an array of three numbers
 })
 
+// /api/courses/1
+app.get('/api/course/:id', (request, response) => {
+  response.send(request.params.id); //requests rout parameters
+  //response.send(req.query); with a query string
+})
+
 //PORT is our environment variable for our port, use this if available otherwise use 3000
 const port = process.env.PORT || 3000;
-
-
 // must have a listener
 app.listen(port, () => console.log(`WORKS listening on port ${port}....`))
