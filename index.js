c7cf3d457006d26ff27e47af414a7c72890e93a4
Simple Dimple John <simpledimplejohn@gmail.com>
2021-11-04 13:11:17 -0700

    add endpoints to the post

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 // a const for calling the expression, called app by convention
 const app = express()
 
+// very important--adds some midleware that makes it so we don't have to change strings to json
+app.use(express.json());
+
+//hard coded array of courses--no database yet
+const courses = [
+  { id: 1, name: 'course1 '},
+  { id: 2, name: 'course2 '},
+  { id: 3, name: 'course3 '},
+]
+
 // available arguments
 // app.get()
 // app.post()
@@ -16,14 +26,28 @@ app.get('/', (request, response) => {
 
 
 app.get('/api/courses', (request, response) => {
-  response.send([1,2,3]); //in place of a database call we are using an array of three numbers
+  response.send(courses); //in place of a database call we are using an array of three numbers
 })
 
 // /api/courses/1
+// how to find a course using the find method, c is courses
 app.get('/api/course/:id', (request, response) => {
-  response.send(request.params.id); //requests rout parameters
-  //response.send(req.query); with a query string
+  const course = courses.find(c => c.id === parseInt(request.params.id))
+  if (!course) response.status(404).send('the course with the id was not found')
+  response.send(course); //works as an else statment
+});
+
+// POST API make a new course
+app.post('/api/courses', (request, response) => {
+  const course = {
+    id: courses.length + 1,
+    name: request.body.name //reads the body object
+  }
+  courses.push(course);
+  response.send(course); //always return this to the client
 })
+
+
 
 //PORT is our environment variable for our port, use this if available otherwise use 3000
 const port = process.env.PORT || 3000;
